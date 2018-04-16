9b04fc29f2146dbe762d65d31618df097c6de636
vladokbln <vladokbln@gmail.com>
2018-04-16 14:42:56 +0300

    clean branch

--- src/js/sections/testimonials.js
+++
@@ -1,7 +0,0 @@
-$('.testimonials__slider').slick({
-    dots: false,
-    arrows: true,
-    autoplay: false,
-    prevArrow: '.testimonials__slider-btn_prev',
-    nextArrow: '.testimonials__slider-btn_next'
-});
